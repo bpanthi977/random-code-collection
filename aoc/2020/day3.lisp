ee2306d355e4c436acbe5e5a00445fd28c754a1c
Bibek Panthi <bpanthi977@gmail.com>
2020-12-03 14:19:25 +0545

    improvements after reading reddit submits
    make-grid: make-array can directly take a list of string as
               initial-contents. I hadn't thought of it.  (from landimatte)
    
    loop count: Why do I keep forgetting it!  (from oantolin)

--- aoc/2020/day3.lisp
+++ aoc/2020/day3.lisp
@@ -4,31 +4,18 @@
 (in-package :aoc3)
 
 (defun make-grid (input)
-  (let ((grid (make-array (list (length input) (length (first input)))
-			  :element-type 'boolean :initial-element nil)))
-    (loop for row in input 
-	  for i from 0 do 
-	  (loop for char across row 
-		for j from 0 do 
-		(setf (aref grid i j) 
-		      (if (eql char #\.)
-			  nil 
-			  t))))
-
-    grid))
+  (make-array (list (length input) (length (first input)))
+	      :initial-contents input))
 
 (defparameter *input* (make-grid (input 3 :lines)))
 
 (defun tree? (i j)
-  (aref *input* i (mod j (array-dimension *input* 1))))
+  (char= #\# (aref *input* i (mod j (array-dimension *input* 1)))))
 
 (defun solve1 (&optional (istep 1) (jstep 3)) 
   (loop for i from 0 below (array-dimension *input* 0) by istep
 	for j from 0 by jstep 
-	with count = 0 do 
-	  (when (tree? i j)
-	    (incf count))
-	finally (return count)))
+	count (tree? i j)))
 
 (defun solve2 () 
   (* (solve1 1 1)
