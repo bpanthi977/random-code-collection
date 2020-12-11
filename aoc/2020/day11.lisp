cd6e6809833fed8342a033524a23491c5eea0200
Bibek Panthi <bpanthi977@gmail.com>
2020-12-11 20:30:50 +0545

    name, order & changes in functions for better readability

--- aoc/2020/day11.lisp
+++ aoc/2020/day11.lisp
@@ -5,26 +5,25 @@
 
 (defparameter *input* (input 11 :lines))
 
-(defun parse-input (input)
-  (let ((grid (make-array (list (length input)
-                                (length (first input)))
-                          :element-type '(member :floor :empty :occupied)
-                          :initial-element :floor)))
-    (loop for row in input
-          for i from 0 do 
-          (loop for cell across row
-                for j from 0 do 
-                (setf (aref grid i j)
-                      (case cell 
-                        (#\. :floor) (#\L :empty) (#\# :occupied)
-                        (t (error "Unknown cell type"))))))
-    grid))
+;;; Utilities
+(defun map-grid-into (newgrid grid function)
+  "map `function' to each element of 2d `grid' and store in `newgrid'"
+  (destructuring-bind (m n) (array-dimensions grid)
+    (loop for i from 0 below m do
+          (loop for j from 0 below n do 
+                (setf (aref newgrid i j)
+                      (funcall function grid i j)))))
+  newgrid)
 
-(defun dir-occupiedp (grid i j dirx diry)
-  (when (array-in-bounds-p grid (+ i dirx) (+ j diry))
-    (eql (aref grid (+ i dirx) (+ j diry)) :occupied)))
+(defun grid-equalp (grid newgrid)
+  (loop for i from 0 below (array-total-size grid)
+        unless (eql (row-major-aref grid i)
+                    (row-major-aref newgrid i))
+          return nil
+        finally (return t)))
 
-(defun count-if-in-dir (predicate grid m n)
+(defun dir-count-if (predicate grid m n)
+  "for each 8 directions from position (m,n) of `grid' count if `predicate' returns true"
   (let ((count 0))
     (loop for dirx from -1 to 1 do
       (loop for diry from -1 to 1 do
@@ -33,8 +32,25 @@
             (incf count)))))
     count))
 
+;;; Parsing
+(defun parse-input (input)
+  (let ((array (make-array (list (length input)
+				 (length (first input)))
+			   :initial-contents input)))
+    (dotimes (i (array-total-size array))
+      (setf (row-major-aref array i)
+	    (case (row-major-aref array i)
+	      (#\. :floor) (#\L :empty) (#\# :occupied)
+              (t (error "Unknown cell type")))))
+    array))
+
+;;; Solution part 1
+(defun occupiedp (grid i j dirx diry)
+  (when (array-in-bounds-p grid (+ i dirx) (+ j diry))
+    (eql (aref grid (+ i dirx) (+ j diry)) :occupied)))
+
 (defun evolve1 (grid i j)
-  (let ((oc (count-if-in-dir #'dir-occupiedp grid i j))
+  (let ((oc (dir-count-if #'occupiedp grid i j))
         (cell (aref grid i j)))
     (cond ((and (eql cell :empty)
                 (= oc 0))
@@ -44,45 +60,32 @@
            :empty)
           (t cell))))
 
-(defun map-grid-into (newgrid grid function)
-  (destructuring-bind (m n) (array-dimensions grid)
-    (loop for i from 0 below m do
-          (loop for j from 0 below n do 
-                (setf (aref newgrid i j)
-                      (funcall function grid i j)))))
-  newgrid)
-
-(defun grid-equal (grid newgrid)
-  (loop for i from 0 below (array-total-size grid)
-        unless (eql (row-major-aref grid i)
-                    (row-major-aref newgrid i))
-          return nil
-        finally (return t)))
-
-(defun solve (evolution-function &optional (input *input*))
-  (let* ((grid (parse-input input))
-         (newgrid (make-array (array-dimensions grid) :initial-element :empty)))
+(defun evolve-till-stable (evolution-function grid)
+  (let ((newgrid (make-array (array-dimensions grid))))
     (loop do 
       (map-grid-into newgrid grid evolution-function)
-      (when (grid-equal grid newgrid)
+      (when (grid-equalp grid newgrid)
         (return))
       (rotatef grid newgrid))
-    (count :occupied (make-array (array-total-size grid) :displaced-to grid))))
+    newgrid))
 
 (defun solve1 ()
-  (solve #'evolve1))
+  (let* ((grid (parse-input *input*))
+	 (solution (evolve-till-stable #'evolve1 grid)))
+    (count :occupied (make-array (array-total-size solution) :displaced-to solution))))
 
-(defun dir-occupiedp2 (grid i j dirx diry)
+;;; Solution part 2
+(defun dir-occupiedp (grid i j dirx diry)
   (let ((newi (+ i dirx))
         (newj (+ j diry)))
     (when (array-in-bounds-p grid newi newj)
       (case (aref grid newi newj)
         (:empty nil)
         (:occupied t)
-        (:floor (dir-occupiedp2 grid newi newj dirx diry))))))
+        (:floor (dir-occupiedp grid newi newj dirx diry))))))
 
 (defun evolve2 (grid i j)
-  (let ((oc (count-if-each-dir #'dir-occupiedp2 grid i j))
+  (let ((oc (dir-count-if #'dir-occupiedp grid i j))
         (cell (aref grid i j)))
     (cond ((and (eql cell :empty)
                 (= oc 0))
@@ -93,6 +96,8 @@
           (t cell))))
 
 (defun solve2 ()
-  (solve #'evolve2))
+  (let* ((grid (parse-input *input*))
+	 (solution (evolve-till-stable #'evolve2 grid)))
+    (count :occupied (make-array (array-total-size solution) :displaced-to solution))))
 
 
