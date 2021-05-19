262737d68f8afd9b740c66fe1baae5dbdbc8e798
Bibek Panthi <bpanthi977@gmail.com>
2021-05-19 20:21:12 +0545

    basic trails (chaotic system)

--- maths/chaotic-system-ball-in-sphere.lisp
+++ maths/chaotic-system-ball-in-sphere.lisp
@@ -2,20 +2,32 @@
 
 (setf *read-default-float-format* 'double-float)
 
-(deftype coord ()
-  `(complex double-float))
+;; Utilities (Ring Buffer)
+(defstruct ring-buffer
+  (buffer nil :type (vector coord))
+  (fill-pointer nil :type fixnum)
+  (count nil :type fixnum))
 
-(deftype color ()
-  `sdl:color)
+(defun initialize-ring-buffer (size)
+  (assert (not (= size 0)))
+  (make-ring-buffer :buffer (make-array size :element-type 'coord :initial-element #C(0.0 0.0))
+		    :fill-pointer 0
+		    :count 0))
 
-(defstruct system
-  (balls nil :type (vector coord))
-  (colors nil :type (vector color))
-  (velocities nil :type (vector coord))
-  (sphere-radius nil :type single-float)
-  (balls-radius nil :type single-float))
+(defun ring-buffer-push (item ring)
+  (with-slots (buffer fill-pointer count) ring
+    (let* ((size (length buffer)))
+      (setf (aref buffer fill-pointer) item
+	    fill-pointer (mod (1+ fill-pointer) size)
+	    count (min size (1+ count))))
+    ring))
 
-(defparameter *gravity* #c(0.0 9.81))
+(defun map-ring-buffer (function ring)
+  (with-slots (buffer fill-pointer count) ring
+    (unless (= count 0)
+      (loop with size = (length buffer)
+	    for i from (1- fill-pointer) downto (- fill-pointer count) do
+	      (funcall function (aref buffer (mod i size)))))))
 
 (defun dot (a b)
   "Dot product of coordinates a and b"
@@ -24,27 +36,68 @@
      (* (imagpart a) (imagpart b))))
 
 ;;;
+;;; Data Structures
+;;;
+(deftype coord ()
+  `(complex double-float))
+
+(deftype color ()
+  `sdl:color)
+
+(defstruct system
+  (balls nil :type (vector coord))
+  (trails nil :type (vector ring-buffer))
+  (colors nil :type (vector color))
+  (velocities nil :type (vector coord))
+  (sphere-radius nil :type single-float)
+  (balls-radius nil :type single-float))
+
+(defparameter *gravity* #c(0.0 9.81))
+
+;;;
 ;;; Drawing
 ;;;
+(defparameter *scale* 200)
+(defparameter *shift* #C(400 250))
+
+(defun transform (coord)
+  (declare (type coord coord))
+  (+ (* *scale* coord) *shift*))
+
+(defun coord->sdl-point (coord)
+  (declare (type coord coord))
+  (sdl:point :x (truncate (realpart coord))
+	     :y (truncate (imagpart coord))))
 
 (defun draw-circle (coord radius &optional (color sdl:*black*))
   (declare (type coord coord)
 	   (type single-float radius)
 	   (type color color))
-  (let* ((scale 200)
-	 (shift #c(400 250))
-	 (coord (+ (* scale coord) shift)))
+  (let* ((coord (transform coord)))
     (sdl:draw-circle-* (truncate (realpart coord))
 		       (truncate (imagpart coord))
-		       (truncate (* scale radius))
+		       (truncate (* *scale* radius))
 		       :color color)))
+
+(defun draw-trail (start ring-buffer color)
+  (declare (type coord start)
+	   (type ring-buffer ring-buffer)
+	   (type color color))
+  (let ((start (coord->sdl-point (transform start))))
+    (map-ring-buffer (lambda (end)
+		       (setf end (coord->sdl-point (transform end)))
+		       (sdl:draw-line start end :color color)
+		       (setf start end))
+		     ring-buffer)))
 
 (defun draw (system)
   "Draws everything i.e. some circles"
   (declare (type system system))
   (loop for ball of-type coord  across (system-balls system)
+	for trail of-type ring-buffer across (system-trails system)
 	for color of-type color across (system-colors system)
 	with radius = (system-balls-radius system) do
+	  (draw-trail ball trail color)
 	  (draw-circle ball radius color))
   (draw-circle #c(0.0 0.0) (system-sphere-radius system))
   (values))
@@ -95,11 +148,13 @@
   (loop for position of-type coord across (system-balls system)
 	for i from 0 
 	for velocity of-type coord across (system-velocities system)
+	for trail of-type ring-buffer across (system-trails system)
 	with radius = (system-balls-radius system) do
 	  (multiple-value-bind (position velocity)
 	      (update-state dt position velocity radius (system-sphere-radius system))
 	    (setf (aref (system-balls system) i) position
-		  (aref (system-velocities system) i) velocity)))
+		  (aref (system-velocities system) i) velocity)
+	    (ring-buffer-push position trail)))
   (values))
 
 ;;;
@@ -118,6 +173,7 @@
   (make-system :balls (coords #C(0.0000001 0.0) #C(0.0 0.0) #C(0.00000001 0.0))
 	       :colors (vector sdl:*red* sdl:*blue* sdl:*green*)
 	       :velocities (coords #C(0.0 0.0) #C(0.0 0.0) #C(0.0 0.0))
+	       :trails (make-array 3 :initial-contents (loop repeat 3 collect (initialize-ring-buffer 100)))
 	       :sphere-radius 1.0f0
 	       :balls-radius 0.05f0))
 
@@ -144,8 +200,8 @@
                ;; Update physics
 	       (let* ((t2 (get-internal-real-time))
 		      (dt (float (speedup (/ (- t2 t1) internal-time-units-per-second)))))
-		 (loop repeat 1
-		       do (update (/ dt 1) system))
+		 (loop repeat 10
+		       do (update (/ dt 10) system))
 		 (setf t1 t2))
                ;; Draw objects
                (draw system)
