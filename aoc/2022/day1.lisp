bd28f6eff11aeec2804d1cf1a6723394dec87e50
Bibek Panthi <bpanthi977@gmail.com>
2022-12-04 15:11:02 +0545

    Refactor Day 1 AOC2022

--- aoc/2022/day1.lisp
+++ aoc/2022/day1.lisp
@@ -1,18 +1,16 @@
 (in-package :aoc)
 
+(defun total-calories ()
+  (let ((calories (split-sequence ""
+                                  (input 01 :lines)
+                                  :test #'string-equal)))
+    (mapcar (lambda (seq)
+              (reduce #'+ seq :key #'parse-integer))
+            calories)))
+
 (defun solve1 ()
-  (let ((totals (mapcar (lambda (seq)
-                          (reduce #'+ seq :key #'parse-integer))
-                        (split-sequence ""
-                                        (input 01 :lines)
-                                        :test #'string-equal))))
-    (apply #'max totals)))
+  (apply #'max (total-calories)))
 
 
 (defun solve2 ()
-  (let ((totals (mapcar (lambda (seq)
-                          (reduce #'+ seq :key #'parse-integer))
-                        (split-sequence ""
-                                        (input 01 :lines)
-                                        :test #'string-equal))))
-    (reduce #'+ (take 3 (sort totals #'>)))))
+  (reduce #'+ (bestn 3 (total-calories) #'>)))
