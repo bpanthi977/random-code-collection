49153c38b40c1ed83b93166856c7c3d1c54fe542
Bibek Panthi <bpanthi977@gmail.com>
2024-05-19 14:55:36 +0545

    ⚡️ Project Euler 91: Faster

--- puzzles/project-euler-91.lisp
+++ puzzles/project-euler-91.lisp
@@ -1,34 +1,35 @@
 ;; https://projecteuler.net/problem=91
 
-(defun dot (a b)
-  (+ (* (car a) (car b))
-     (* (cdr a) (cdr b))))
 
+(declaim (inline dot))
+(defun dot (ax ay bx by cx cy)
+  (declare (type fixnum ax ay bx by cx cy))
+  (the fixnum
+       (+ (the fixnum (* (- ax bx) (- bx cx)))
+          (the fixnum (* (- ay by) (- by cy))))))
 
-(defun diff (a b)
-  (cons (- (car a) (car b))
-        (- (cdr a) (cdr b))))
+(defun is-right-angled (px py qx qy)
+  (declare (type fixnum px py qx qy))
+  (or (= (dot px py qx qy  0  0) 0)
+      (= (dot qx qy  0  0 px py) 0)
+      (= (dot  0  0 px py qx qy ) 0)))
 
-(defun is-right-angled (a b c)
-  (or (= (dot (diff a b) (diff b c)) 0)
-      (= (dot (diff b c) (diff c a)) 0)
-      (= (dot (diff c a) (diff a b)) 0)))
-
-(defun is-degenerate (a b c)
-  (or (equal a b)
-      (equal b c)
-      (equal c a)))
+(defun is-degenerate (px py qx qy)
+  (declare (type fixnum px py qx qy))
+  (or (and (= px qx) (= py qy))
+      (= px py 0)
+      (= qx qy 0)))
 
 (defun solve (max)
-  (let ((count 0)
-        (origin (cons 0 0)))
-    (loop for px from 0 to max do
+  (let ((count 0))
+    (declare (type fixnum count))
+    (loop for px fixnum from 0 to max do
           (loop for py from (if (= px 0) 1 0) to max do
                 (loop for qx from  0 to max do
                       (loop for qy from (if (= qx 0) 1 0) to max
                             for q = (cons qx qy)
                             for p = (cons px py) do
-                            (when (and (is-right-angled origin p q)
-                                       (not (is-degenerate origin p q)))
+                            (when (and (is-right-angled px py qx qy)
+                                       (not (is-degenerate px py qx qy)))
                               (incf count))))))
     (/ count 2)))
